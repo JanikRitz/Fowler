ed889ef1a25c5c6b6d26adfc71a8b0546be342eb
JanikRitz <janikritz@gmail.com>
2017-05-03 09:54:15 +0200

    Basic Test for Movie.java

--- Tests/MovieTest.java
+++ Tests/MovieTest.java
@@ -7,20 +7,32 @@ import static org.junit.Assert.*;
  * Created by Janik on 03.05.2017.
  */
 public class MovieTest {
+    static final int priceCodeTest = 3;
+    static final int priceCodeTestPre = -1;
+    static final String testTitle = "Title";
+
     @Before
     public void setUp() throws Exception {
     }
 
     @Test
     public void getPriceCode() throws Exception {
+        Movie m = new Movie(testTitle, priceCodeTest);
+        assertEquals(priceCodeTest, m.getPriceCode());
     }
 
     @Test
     public void setPriceCode() throws Exception {
+        Movie m = new Movie(testTitle, priceCodeTestPre);
+        assertEquals(priceCodeTestPre, m.getPriceCode());
+        m.setPriceCode(priceCodeTest);
+        assertEquals(priceCodeTest, m.getPriceCode());
     }
 
     @Test
     public void getTitle() throws Exception {
+        Movie m = new Movie(testTitle, priceCodeTest);
+        assertEquals(testTitle, m.getTitle());
     }
 
 }
