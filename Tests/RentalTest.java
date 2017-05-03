3d97e64edb480850a26c0daff1bde6452ce7001d
JanikRitz <janikritz@gmail.com>
2017-05-03 09:57:40 +0200

    Basic Test for Rental.java

---
+++ Tests/RentalTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Janik on 03.05.2017.
+ */
+public class RentalTest {
+    private static final int priceCodeTest = 3;
+    private static final int daysTest = 3;
+    private static final String testTitle = "Title";
+    private static final Movie m = new Movie(testTitle, priceCodeTest);
+
+    @Test
+    public void getDaysRented() throws Exception {
+        Rental rental = new Rental(m, daysTest);
+        assertEquals(daysTest, rental.getDaysRented());
+    }
+
+    @Test
+    public void getMovie() throws Exception {
+        Rental rental = new Rental(m, daysTest);
+        assertEquals(m, rental.getMovie());
+    }
+
+}
