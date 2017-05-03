1316aa1c200cfbe72a4f18f8a081038c8c6a813a
JanikRitz <janikritz@gmail.com>
2017-05-03 09:49:05 +0200

    Added JUnit Construct for MovieTest

---
+++ Tests/MovieTest.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Janik on 03.05.2017.
+ */
+public class MovieTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getPriceCode() throws Exception {
+    }
+
+    @Test
+    public void setPriceCode() throws Exception {
+    }
+
+    @Test
+    public void getTitle() throws Exception {
+    }
+
+}
