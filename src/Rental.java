a3a726c8342226c723a73641158e73f83c2bc1b9
JanikRitz <janikritz@gmail.com>
2017-05-03 11:14:01 +0200

    Moved Calculations of getCharge to Movie

--- src/Rental.java
+++ src/Rental.java
@@ -1,31 +1,10 @@
 class Rental {
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(Movie newMovie, int newDaysRented) {
         movie = newMovie;
         daysRented = newDaysRented;
-    }
-
-    public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-
     }
 
     public int getDaysRented() {
@@ -34,6 +13,10 @@ class Rental {
 
     public Movie getMovie() {
         return movie;
+    }
+
+    public double getCharge(){
+        return movie.getCharge(daysRented);
     }
 
     String getString() {
